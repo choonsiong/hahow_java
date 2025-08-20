a48166548f41848d3ad32005b51cdb9bb67f907b
choonsiong <choonsiong.lee@gmail.com>
2025-08-20 17:59:15 +0800

    Auto commit

---
+++ bookmall/src/main/java/org/example/bookmall/BookmallApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmallApplication.class, args);
+    }
+
+}
